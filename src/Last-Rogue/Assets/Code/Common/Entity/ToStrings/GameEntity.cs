4d1378a933494ae0450766563452b3c4fc7ba9b6
Vladyslav <vladyslav.hutsenko@pollub.edu.pl>
2025-02-13 23:03:43 +0100

    Added base architecture with assets, imported Zenject, added Entitas

---
+++ src/Last-Rogue/Assets/Code/Common/Entity/ToStrings/GameEntity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Code.Common.Entity.ToStrings;
+using Entitas;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+public sealed partial class GameEntity : INamedEntity
+{
+  private EntityPrinter _printer;
+
+  public override string ToString()
+  {
+    if (_printer == null)
+      _printer = new EntityPrinter(this);
+
+    _printer.InvalidateCache();
+
+    return _printer.BuildToString();
+  }
+
+  public string EntityName(IComponent[] components)
+  {
+    try
+    {
+      if (components.Length == 1)
+        return components[0].GetType().Name;
+
+      foreach (IComponent component in components)
+      {
+       
+      }
+    }
+    catch (Exception exception)
+    {
+      Debug.LogError(exception.Message);
+    }
+
+    return components.First().GetType().Name;
+  }
+
+  public string BaseToString() => base.ToString();
+}
