06d54f73d9a456d57a7138f2d82c2d6bb21a897d
Vladyslav <vladyslav.hutsenko@pollub.edu.pl>
2025-02-16 17:42:30 +0100

    Changed name of project

---
+++ src/ecs-survivors/Assets/Code/Gameplay/Common/CommonComponents.cs
@@ -0,0 +1,8 @@
+﻿using Entitas;
+using UnityEngine;
+
+namespace Code.Gameplay.Common
+{
+    [Game] public class Id : IComponent { public int Value; }
+    [Game] public class WorldPosition : IComponent { public Vector3 Value; }
+}
--- src/ecs-survivors/Assets/Code/Generated/Game/GameComponentsLookup.cs
+++ src/ecs-survivors/Assets/Code/Generated/Game/GameComponentsLookup.cs
@@ -8,15 +8,18 @@
 //------------------------------------------------------------------------------
 public static class GameComponentsLookup {
 
+    public const int Id = 0;
+    public const int WorldPosition = 1;
 
-
-    public const int TotalComponents = 0;
+    public const int TotalComponents = 2;
 
     public static readonly string[] componentNames = {
-
+        "Id",
+        "WorldPosition"
     };
 
     public static readonly System.Type[] componentTypes = {
-
+        typeof(Code.Gameplay.Common.Id),
+        typeof(Code.Gameplay.Common.WorldPosition)
     };
 }
